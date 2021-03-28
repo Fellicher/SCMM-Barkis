8c2547bf6229b0270077c90db7d7fc5fec32502f
Fellicher <tabis1993@gmail.com>
2021-03-28 20:48:13 +0300

    BIG UPDATE INSANE

--- commands/market.js
+++ commands/market.js
@@ -1,23 +1,28 @@
 const i18n = require("i18n");
 const { LOCALE } = require("../util/EvobotUtil");
-const fs = require("fs");
 const fetch = require("node-fetch");
+const { MessageEmbed } = require("discord.js");
 
 i18n.setLocale(LOCALE);
 
 module.exports = {
   name: "market",
+  aliases: ["m", "price"],
   description: i18n.__('Something about market'),
   async execute(message, args) {
     let data;
     const item = args.join("%20");
 
-    try {
-        var respone = await fetch('https://scmm.app/api/market/item/'+item);
-        data = await response.json();
-    } catch(e) {
-        console.error(e);
-    }
-    console.log(data);
+    await fetch('https://scmm.app/api/market/item/'+item).then(res => res.json()).then(response => {
+      const embed = new MessageEmbed()
+        .setColor(response.foregroundColour)
+        .setTitle(response.name)
+        .addFields(
+          {name: 'Current price', value: response.buyNowPrice},
+          {name: 'Average price', value: response.allTimeAverageValue}
+        )
+        .setImage(response.iconUrl);
+        message.channel.send(embed);
+    });
   }
 };
